e26ba89cb19ba74008b2296325442560bf4a116d
Igor Kolosov <igorkol@users.noreply.github.com>
2020-08-27 10:38:14 +0200

    Feature/check littledata layer (#9)
    * Add prettifier and linter
    
    * added lint
    
    * made some adjustments to the setup
    
    * Revert "made some adjustments to the setup"
    
    This reverts commit 98688aee1862fd271a9a4b93220ef83a15a89412.
    
    * made config changes
    
    * cleaned background.js from errors
    
    * cleaner all the background errors
    
    * correcting issues that appeared after lint install
    
    * ld datalayer check exists now
    
    * making the injected js readable by prettify
    
    * gitignore and code reformatting
    
    Co-authored-by: Edward Upton <edward@littledata.io>

--- content.start.js
+++ content.start.js
@@ -9,203 +9,243 @@
   Values defined or populated via this script are available for content.idle.js to read and edit, though.
  */
 
-chrome.storage.local.get('state', function(result){
-  if(result.state === null) {
-    initDisabledState();
-  } else if (result.state === false) {
-    initDisabledState();
-  } if(result.state === true) {
-    initActiveState();
-    injectContentScriptJS();
-    initPageDataContentListener();
-  }
+chrome.storage.local.get('state', function(result) {
+	if (result.state === null) {
+		initDisabledState();
+	} else if (result.state === false) {
+		initDisabledState();
+	}
+	if (result.state === true) {
+		initActiveState();
+		injectContentScriptJS();
+		initPageDataContentListener();
+	}
 });
- 
+
 //Page render with extension active state == false
 function initDisabledState() {
-   chrome.runtime.sendMessage({ from: 'content', subject: 'changeExtensionIcon', mode: false });
+	chrome.runtime.sendMessage({
+		from: 'content',
+		subject: 'changeExtensionIcon',
+		mode: false,
+	});
 }
 
 //Page render with extension active state == true
 function initActiveState() {
-   chrome.runtime.sendMessage({ from: 'content', subject: 'changeExtensionIcon', mode: true });
+	chrome.runtime.sendMessage({
+		from: 'content',
+		subject: 'changeExtensionIcon',
+		mode: true,
+	});
 }
 
+//injecting code that does the checks in the client window
 function injectContentScriptJS() {
-  let injectCode = `
-    function getLittlebugPageData() {
+	const injectCode =
+		'(' +
+		function() {
+			function getLittlebugPageData() {
+				//OUTPUT OBJECT
+				var data = {
+					href: window.location.href || '',
+					Littledata: {
+						hasLittledataLayer: false,
+						hasTrackingTag: false,
+						hasGATrackerJS: false,
+						hasSegmentTrackerJS: false,
+						hasCarthookTrackerJS: false,
+						version: '',
+						webPropertyID: '',
+					},
+					CookieID: '',
+					ClientID: '',
+					CartClientID: '',
+					Scripts: {
+						classic: false,
+						universal: false,
+						doubleclick: false,
+						gtag: false,
+						gtm: false,
+					},
+				};
 
-      //OUTPUT OBJECT
-      var data = {
-        href : window.location.href || '',
-        Littledata : {
-          hasLittledataLayer : false,
-          hasTrackingTag : false,
-          hasGATrackerJS : false,
-          hasSegmentTrackerJS : false,
-          hasCarthookTrackerJS : false,
-          version : '',
-          webPropertyID : ''
-        },
-        CookieID : '',
-        ClientID : '',
-        CartClientID : '',
-        Scripts  : {
-          classic : false,
-          universal : false,
-          doubleclick : false,
-          gtag : false,
-          gtm : false
-        }
-      };
-      
-      //What scripts are on this page?
-      let scripts = document.getElementsByTagName('script');
-      let rgxGA = /www\\.google-analytics\\.com\\/ga\\.js/;
-      let rgxUA = /www\\.google-analytics\\.com\\/analytics\\.js/;
-      let rgxDC = /stats\\.g\.doubleclick\\.net\\/dc\\.js/;
-      let rgxGT = /www\\.googletagmanager\\.com\\/gtag\\/js/;
-      let rgxTM = /www\\.googletagmanager\\.com\\/gtm\\.js/;
-      let rgxLDC = /cdn\\.jsdelivr\\.net\\/gh\\/littledata\\/shopify-tracker\\/dist\\/carthookTracker\\.js/;
-      let rgxLDG = /cdn\\.jsdelivr\\.net\\/gh\\/littledata\\/shopify-tracker\\/dist\\/gaTracker\\.js/;
-      let rgxLDS = /cdn\\.jsdelivr\\.net\\/gh\\/littledata\\/shopify-tracker\\/dist\\/segmentTracker\\.js/;
+				//What scripts are on this page?
+				let scripts = document.getElementsByTagName('script');
+				let rgxGA = /www\.google\-analytics\.com\/ga\.js/;
+				let rgxUA = /www\.google\-analytics\.com\/analytics\.js/;
+				let rgxDC = /stats\.g\.doubleclick\.net\/dc\.js/;
+				let rgxGT = /www\.googletagmanager\.com\/gtag\/js/;
+				let rgxTM = /www\.googletagmanager\.com\/gtm\.js/;
+				let rgxLDC = /cdn\.jsdelivr\.net\/gh\/littledata\/shopify\-tracker\/dist\/carthookTracker\.js/;
+				let rgxLDG = /cdn\.jsdelivr\.net\/gh\/littledata\/shopify\-tracker\/dist\/gaTracker\.js/;
+				let rgxLDS = /cdn\.jsdelivr\.net\/gh\/littledata\/shopify\-tracker\/dist\/segmentTracker\.js/;
 
-      for (var i = 0, len = scripts.length; i < len; i++) {
-        if (rgxGA.test(scripts[i].src)) data.Scripts.classic = true;
-        if (rgxUA.test(scripts[i].src)) data.Scripts.universal = true;
-        if (rgxDC.test(scripts[i].src)) data.Scripts.doubleclick = true;
-        if (rgxGT.test(scripts[i].src)) data.Scripts.gtag = true;
-        if (rgxTM.test(scripts[i].src)) data.Scripts.gtm = true;
-        if (rgxLDC.test(scripts[i].src)) data.Littledata.hasCarthookTrackerJS = true;
-        if (rgxLDG.test(scripts[i].src)) data.Littledata.hasGATrackerJS = true;
-        if (rgxLDS.test(scripts[i].src)) data.Littledata.hasSegmentTrackerJS = true;
-      }
+				for (var i = 0, len = scripts.length; i < len; i++) {
+					if (rgxGA.test(scripts[i].src)) data.Scripts.classic = true;
+					if (rgxUA.test(scripts[i].src))
+						data.Scripts.universal = true;
+					if (rgxDC.test(scripts[i].src))
+						data.Scripts.doubleclick = true;
+					if (rgxGT.test(scripts[i].src)) data.Scripts.gtag = true;
+					if (rgxTM.test(scripts[i].src)) data.Scripts.gtm = true;
+					if (rgxLDC.test(scripts[i].src))
+						data.Littledata.hasCarthookTrackerJS = true;
+					if (rgxLDG.test(scripts[i].src))
+						data.Littledata.hasGATrackerJS = true;
+					if (rgxLDS.test(scripts[i].src))
+						data.Littledata.hasSegmentTrackerJS = true;
+				}
 
-      //What version of LD script is running and to what property?
-      if(window.LittledataLayer) {
-        data.Littledata.hasLittledataLayer = true;
-        data.Littledata.version = window.LittledataLayer.version;
-        data.Littledata.webPropertyID = window.LittledataLayer.webPropertyID;
-      }
-      
-      try {
-        let rgxTagVersion = /\\/[\\/\\*]\\s*Version\\s+(v?\\d+\.\\d+(?:\\.\\d+){0,2})/gi;
-        let rgxTagProperty = /UA\\-[0-9]+\\-[0-9]+/
+				//What version of LD script is running and to what property?
+				if (window.LittledataLayer) {
+					data.Littledata.hasLittledataLayer = true;
+					data.Littledata.version = window.LittledataLayer.version;
+					data.Littledata.webPropertyID =
+						window.LittledataLayer.webPropertyID;
+				}
 
-        let ldTagElem = document.querySelector('[name="littledata-tracking-tag"');
-        if (ldTagElem) {
-          data.Littledata.hasTrackingTag = true;
-          var src = ldTagElem.text;
-          
-          if (data.Littledata.version === '') {
-            //Get version number
-            let v = rgxTagVersion.exec(src);
-            if (v && v[1]) data.Littledata.version = v[1];
-          }
+				try {
+					let rgxTagVersion = /\/[\/\*]\s*Version\s+(v?\d+\.\d+(?:\.\d+){0,2})/gi;
+					let rgxTagProperty = /UA\-[0-9]+\-[0-9]+/;
 
-          if (data.Littledata.webPropertyID == undefined) {
-            //Get web property ID
-            let p = rgxTagProperty.exec(src);
-            if (p) {
-              data.Littledata.webPropertyID = p[0];
-            } else {
-              data.Littledata.webPropertyID = ga.getAll()[0].get('trackingId');
-            }
-          }
-        } else {
-          console.debug('Could not find LD tag');
-        }
-      } catch(e) {}
+					let ldTagElem = document.querySelector(
+						'[name="littledata-tracking-tag"'
+					);
+					if (ldTagElem) {
+						data.Littledata.hasTrackingTag = true;
+						var src = ldTagElem.text;
 
-      //Get Client ID from _ga Cookie
-      try {
-        let Cookie = "; " + document.cookie;
-        let parts = Cookie.split("; _ga=");
-        if (parts.length == 2) {
-          let CookieText = parts.pop().split(';').shift();
-          let CookieValues = CookieText.split('.');
-          if (CookieValues.length == 4)
-            data.CookieID = CookieValues[2] + '.' + CookieValues[3];
-        }
-      } catch(e) {}
-      
-      //Get Client ID ga global object
-      try {
-        data.ClientID = ga.getAll()[0].get('clientId');
-      } catch(e) {}
-      
-      
-      //Get Client ID from Cart data
-      
-      // *** ISSUE ***: This doesn't seem to be saving to the log in local storage, perhaps a sync issue with when it's saved vs the ajax call?
-      
-      try {
-        let xhr = new XMLHttpRequest();
-        let googleClientID;
-        xhr.open('GET', '/cart.js', false);
+						if (data.Littledata.version === '') {
+							//Get version number
+							let v = rgxTagVersion.exec(src);
+							if (v && v[1]) data.Littledata.version = v[1];
+						}
 
-        xhr.onreadystatechange = function (oEvent) {
-          if (xhr.readyState === 4) {
-            //Ignore 404s etc. so we don't get console error messages showing up
-            if (xhr.status === 200) {
-              if (xhr.responseText) {
-                var cartData = JSON.parse(xhr.responseText);
-                if (cartData && cartData.attributes) {
-                  googleClientID = cartData.attributes['google-clientID'];
-                  if (googleClientID) {
-                    data.CartClientID = googleClientID;
-                  } else {
-                    //fallback for older versions
-                    googleClientID = cartData.attributes['clientID'];
-                    if (googleClientID) {
-                      data.CartClientID = googleClientID;
-                      console.debug('CCID in legacy');
-                    }
-                  }
-                }
-              }
-            }
-          }
-        };
-        xhr.send();
-      } catch(err) {
-        console.debug("Littlebug XHR error: ",err.message);
-      }
-      
-      return data;
-    }
+						if (data.Littledata.webPropertyID == undefined) {
+							//Get web property ID
+							let p = rgxTagProperty.exec(src);
+							if (p) {
+								data.Littledata.webPropertyID = p[0];
+							} else {
+								data.Littledata.webPropertyID = ga
+									.getAll()[0]
+									.get('trackingId');
+							}
+						}
+					} else {
+						console.debug('Could not find LD tag');
+					}
+				} catch (e) {}
 
-    window.addEventListener(
-      "load",
-      function(event) {
-        var data = getLittlebugPageData();
-        window.dispatchEvent(new CustomEvent("setLittledataPageData", {
-          detail: data
-        }));
-        var logLD = "LD extension:" + JSON.stringify(data, null, 2);
-        console.debug(logLD);
-      }
-    );
-    
-    // setting an internal visitor filter for GA
-    if (window.location.hostname.indexOf("littledata.io") > -1) {
-      window.localStorage.setItem("LD internal","yes");
-    }
-    
-  `;
+				//Get Client ID from _ga Cookie
+				try {
+					let Cookie = '; ' + document.cookie;
+					let parts = Cookie.split('; _ga=');
+					if (parts.length == 2) {
+						let CookieText = parts
+							.pop()
+							.split(';')
+							.shift();
+						let CookieValues = CookieText.split('.');
+						if (CookieValues.length == 4)
+							data.CookieID =
+								CookieValues[2] + '.' + CookieValues[3];
+					}
+				} catch (e) {}
 
-  const script = document.createElement('script');
-  script.textContent = injectCode;
-  document.documentElement.appendChild(script);
-  script.parentNode.removeChild(script);
+				//Get Client ID ga global object
+				try {
+					data.ClientID = ga.getAll()[0].get('clientId');
+				} catch (e) {}
+
+				//Get Client ID from Cart data
+
+				// *** ISSUE ***: This doesn't seem to be saving to the log in local storage, perhaps a sync issue with when it's saved vs the ajax call?
+
+				try {
+					let xhr = new XMLHttpRequest();
+					let googleClientID;
+					xhr.open('GET', '/cart.js', false);
+
+					xhr.onreadystatechange = function(oEvent) {
+						if (xhr.readyState === 4) {
+							//Ignore 404s etc. so we don't get console error messages showing up
+							if (xhr.status === 200) {
+								if (xhr.responseText) {
+									var cartData = JSON.parse(xhr.responseText);
+									if (cartData && cartData.attributes) {
+										googleClientID =
+											cartData.attributes[
+												'google-clientID'
+											];
+										if (googleClientID) {
+											data.CartClientID = googleClientID;
+										} else {
+											//fallback for older versions
+											googleClientID =
+												cartData.attributes['clientID'];
+											if (googleClientID) {
+												data.CartClientID = googleClientID;
+												console.debug('CCID in legacy');
+											}
+										}
+									}
+								}
+							}
+						}
+					};
+					xhr.send();
+				} catch (err) {
+					console.debug('Littlebug XHR error: ', err.message);
+				}
+
+				return data;
+			}
+
+			window.addEventListener('load', function(event) {
+				var data = getLittlebugPageData();
+				if (data.Littledata.hasLittledataLayer) {
+					window.dispatchEvent(
+						new CustomEvent('setLittledataPageData', {
+							detail: data,
+						})
+					);
+				}
+
+				var logLD = 'LD extension:' + JSON.stringify(data, null, 2);
+				console.debug(logLD);
+			});
+
+			// setting an internal visitor filter for GA
+			if (window.location.hostname.indexOf('littledata.io') > -1) {
+				window.localStorage.setItem('LD internal', 'yes');
+			}
+		} +
+		')();';
+
+	const script = document.createElement('script');
+	script.textContent = injectCode;
+	document.documentElement.appendChild(script);
+	script.parentNode.removeChild(script);
 }
 
 function initPageDataContentListener() {
-  let pageLog = [];
-  chrome.storage.local.get('pageLog', function(result) { pageLog = JSON.parse(result.pageLog); });
-  
-  window.addEventListener("setLittledataPageData", function(data) {
-    pageLog.push(data.detail); //add current page's data to the log
-    chrome.runtime.sendMessage({ from: 'content', subject: 'savePageLogData', data: JSON.stringify(pageLog) });
-  }, false);
+	let pageLog = [];
+	chrome.storage.local.get('pageLog', function(result) {
+		pageLog = JSON.parse(result.pageLog);
+	});
+
+	window.addEventListener(
+		'setLittledataPageData',
+		function(data) {
+			pageLog.push(data.detail); //add current page's data to the log
+			chrome.runtime.sendMessage({
+				from: 'content',
+				subject: 'savePageLogData',
+				data: JSON.stringify(pageLog),
+			});
+		},
+		false
+	);
 }
