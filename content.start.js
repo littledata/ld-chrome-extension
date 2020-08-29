2ae1cfc2da006516af0eb85b9fb54f8a86137c2f
Igor Kolosov <igorkol@users.noreply.github.com>
2020-08-29 22:38:31 +0200

    Bug/ld check corrections (#11)
    * Add prettifier and linter
    
    * added lint
    
    * made some adjustments to the setup
    
    * Revert "made some adjustments to the setup"
    
    This reverts commit 98688aee1862fd271a9a4b93220ef83a15a89412.
    
    * made config changes
    
    * cleaned background.js from errors
    
    * cleaner all the background errors
    
    * correcting issues that appeared after lint install
    
    * ld datalayer check exists now
    
    * making the injected js readable by prettify
    
    * gitignore and code reformatting
    
    * resolved .gitignore issue
    
    * removed .gitignore too
    
    * made multiple corrections
    
    * gitignore
    
    * gitignore reset
    
    * another removal of cached files
    
    Co-authored-by: Edward Upton <edward@littledata.io>

--- content.start.js
+++ content.start.js
@@ -47,7 +47,8 @@ function injectContentScriptJS() {
 		function() {
 			function getLittlebugPageData() {
 				//OUTPUT OBJECT
-				var data = {
+
+				let data = {
 					href: window.location.href || '',
 					Littledata: {
 						hasLittledataLayer: false,
@@ -70,151 +71,161 @@ function injectContentScriptJS() {
 					},
 				};
 
-				//What scripts are on this page?
-				let scripts = document.getElementsByTagName('script');
-				let rgxGA = /www\.google\-analytics\.com\/ga\.js/;
-				let rgxUA = /www\.google\-analytics\.com\/analytics\.js/;
-				let rgxDC = /stats\.g\.doubleclick\.net\/dc\.js/;
-				let rgxGT = /www\.googletagmanager\.com\/gtag\/js/;
-				let rgxTM = /www\.googletagmanager\.com\/gtm\.js/;
-				let rgxLDC = /cdn\.jsdelivr\.net\/gh\/littledata\/shopify\-tracker\/dist\/carthookTracker\.js/;
-				let rgxLDG = /cdn\.jsdelivr\.net\/gh\/littledata\/shopify\-tracker\/dist\/gaTracker\.js/;
-				let rgxLDS = /cdn\.jsdelivr\.net\/gh\/littledata\/shopify\-tracker\/dist\/segmentTracker\.js/;
-
-				for (var i = 0, len = scripts.length; i < len; i++) {
-					if (rgxGA.test(scripts[i].src)) data.Scripts.classic = true;
-					if (rgxUA.test(scripts[i].src))
-						data.Scripts.universal = true;
-					if (rgxDC.test(scripts[i].src))
-						data.Scripts.doubleclick = true;
-					if (rgxGT.test(scripts[i].src)) data.Scripts.gtag = true;
-					if (rgxTM.test(scripts[i].src)) data.Scripts.gtm = true;
-					if (rgxLDC.test(scripts[i].src))
-						data.Littledata.hasCarthookTrackerJS = true;
-					if (rgxLDG.test(scripts[i].src))
-						data.Littledata.hasGATrackerJS = true;
-					if (rgxLDS.test(scripts[i].src))
-						data.Littledata.hasSegmentTrackerJS = true;
-				}
-
 				//What version of LD script is running and to what property?
 				if (window.LittledataLayer) {
 					data.Littledata.hasLittledataLayer = true;
 					data.Littledata.version = window.LittledataLayer.version;
 					data.Littledata.webPropertyID =
 						window.LittledataLayer.webPropertyID;
-				}
 
-				try {
-					let rgxTagVersion = /\/[\/\*]\s*Version\s+(v?\d+\.\d+(?:\.\d+){0,2})/gi;
-					let rgxTagProperty = /UA\-[0-9]+\-[0-9]+/;
 
-					let ldTagElem = document.querySelector(
-						'[name="littledata-tracking-tag"'
-					);
-					if (ldTagElem) {
-						data.Littledata.hasTrackingTag = true;
-						var src = ldTagElem.text;
+					//What scripts are on this page?
+					let scripts = document.getElementsByTagName('script');
+					let rgxGA = /www\.google\-analytics\.com\/ga\.js/;
+					let rgxUA = /www\.google\-analytics\.com\/analytics\.js/;
+					let rgxDC = /stats\.g\.doubleclick\.net\/dc\.js/;
+					let rgxGT = /www\.googletagmanager\.com\/gtag\/js/;
+					let rgxTM = /www\.googletagmanager\.com\/gtm\.js/;
+					let rgxLDC = /.+dist\/carthookTracker\.js/;
+					let rgxLDG = /.+dist\/gaTracker\.js/;
+					let rgxLDS = /.+dist\/segmentTracker\.js/;
 
-						if (data.Littledata.version === '') {
-							//Get version number
-							let v = rgxTagVersion.exec(src);
-							if (v && v[1]) data.Littledata.version = v[1];
-						}
+					for (var i = 0, len = scripts.length; i < len; i++) {
+						if (rgxGA.test(scripts[i].src))
+							data.Scripts.classic = true;
+						if (rgxUA.test(scripts[i].src))
+							data.Scripts.universal = true;
+						if (rgxDC.test(scripts[i].src))
+							data.Scripts.doubleclick = true;
+						if (rgxGT.test(scripts[i].src))
+							data.Scripts.gtag = true;
+						if (rgxTM.test(scripts[i].src)) data.Scripts.gtm = true;
+						if (rgxLDC.test(scripts[i].src))
+							data.Littledata.hasCarthookTrackerJS = true;
+						if (rgxLDG.test(scripts[i].src))
+							data.Littledata.hasGATrackerJS = true;
+						if (rgxLDS.test(scripts[i].src))
+							data.Littledata.hasSegmentTrackerJS = true;
+					}
 
-						if (data.Littledata.webPropertyID == undefined) {
-							//Get web property ID
-							let p = rgxTagProperty.exec(src);
-							if (p) {
-								data.Littledata.webPropertyID = p[0];
-							} else {
-								data.Littledata.webPropertyID = ga
-									.getAll()[0]
-									.get('trackingId');
+					// KNOWN BUG: there is a race with GA object and sometimes we don't get tracking ID
+					// or client ID in time
+
+					try {
+						let rgxTagVersion = /\/[\/\*]\s*Version\s+(v?\d+\.\d+(?:\.\d+){0,2})/gi;
+						let rgxTagProperty = /UA\-[0-9]+\-[0-9]+/;
+
+						let ldTagElem = document.querySelector(
+							'[name="littledata-tracking-tag"]'
+						);
+						if (ldTagElem) {
+							data.Littledata.hasTrackingTag = true;
+							var src = ldTagElem.text;
+
+							if (data.Littledata.version === '') {
+								//Get version number
+								let v = rgxTagVersion.exec(src);
+								if (v && v[1]) data.Littledata.version = v[1];
 							}
-						}
-					} else {
-						console.debug('Could not find LD tag');
-					}
-				} catch (e) {}
 
-				//Get Client ID from _ga Cookie
-				try {
-					let Cookie = '; ' + document.cookie;
-					let parts = Cookie.split('; _ga=');
-					if (parts.length == 2) {
-						let CookieText = parts
-							.pop()
-							.split(';')
-							.shift();
-						let CookieValues = CookieText.split('.');
-						if (CookieValues.length == 4)
-							data.CookieID =
-								CookieValues[2] + '.' + CookieValues[3];
-					}
-				} catch (e) {}
-
-				//Get Client ID ga global object
-				try {
-					data.ClientID = ga.getAll()[0].get('clientId');
-				} catch (e) {}
-
-				//Get Client ID from Cart data
-
-				// *** ISSUE ***: This doesn't seem to be saving to the log in local storage, perhaps a sync issue with when it's saved vs the ajax call?
-
-				try {
-					let xhr = new XMLHttpRequest();
-					let googleClientID;
-					xhr.open('GET', '/cart.js', false);
-
-					xhr.onreadystatechange = function(oEvent) {
-						if (xhr.readyState === 4) {
-							//Ignore 404s etc. so we don't get console error messages showing up
-							if (xhr.status === 200) {
-								if (xhr.responseText) {
-									var cartData = JSON.parse(xhr.responseText);
-									if (cartData && cartData.attributes) {
-										googleClientID =
-											cartData.attributes[
-												'google-clientID'
-											];
-										if (googleClientID) {
-											data.CartClientID = googleClientID;
-										} else {
-											//fallback for older versions
-											googleClientID =
-												cartData.attributes['clientID'];
-											if (googleClientID) {
-												data.CartClientID = googleClientID;
-												console.debug('CCID in legacy');
-											}
-										}
-									}
+							if (data.Littledata.webPropertyID == undefined) {
+								//Get web property ID
+								let p = rgxTagProperty.exec(src);
+								if (p) {
+									data.Littledata.webPropertyID = p[0];
+								} else {
+									data.Littledata.webPropertyID = ga
+										.getAll()[0]
+										.get('trackingId');
 								}
 							}
+						} else {
+							console.debug('Could not find LD tag');
 						}
-					};
-					xhr.send();
-				} catch (err) {
-					console.debug('Littlebug XHR error: ', err.message);
-				}
+					} catch (e) {}
 
-				return data;
+					//Get Client ID from _ga Cookie
+					try {
+						let Cookie = '; ' + document.cookie;
+						let parts = Cookie.split('; _ga=');
+						if (parts.length == 2) {
+							let CookieText = parts
+								.pop()
+								.split(';')
+								.shift();
+							let CookieValues = CookieText.split('.');
+							if (CookieValues.length == 4)
+								data.CookieID =
+									CookieValues[2] + '.' + CookieValues[3];
+						}
+					} catch (e) {}
+
+					//Get Client ID ga global object
+					try {
+						data.ClientID = ga.getAll()[0].get('clientId');
+					} catch (e) {}
+
+					//Get Client ID from Cart data
+					// KNOWN BUG: sometimes transactions.littledata.io does not push GA client ID to cart
+					// fast enough after page load before we fetch the cart object
+
+					function getCIDFromCart(cartCIDInput) {
+						let googleClientID;
+						if (cartCIDInput.attributes) {
+							googleClientID =
+								cartCIDInput.attributes['google-clientID'];
+							if (googleClientID) {
+								data.CartClientID = googleClientID;
+							} else {
+								googleClientID =
+									cartCIDInput.attributes['clientID'];
+								if (googleClientID) {
+									data.CartClientID = googleClientID;
+									console.debug('legacy CCID');
+								}
+							}
+							console.debug(
+								'LD extension output:',
+								JSON.stringify(data, null, 2)
+							);
+						}
+					}
+
+					// Fetch cart.js and then dispatch event for backend
+
+					fetch('/cart.js')
+						.then(response => {
+							return response.json().then(cartData => {
+								if (response.ok) {
+									return getCIDFromCart(cartData);
+								} else {
+									return Promise.reject({
+										status: response.status,
+										cartData,
+									});
+								}
+							});
+						})
+						.then(() => {
+							if (data.Littledata.hasLittledataLayer) {
+								window.dispatchEvent(
+									new CustomEvent('setLittledataPageData', {
+										detail: data,
+									})
+								);
+							}
+						})
+						.catch(error =>
+							console.debug('Littlebug fetch error: ', error)
+						);
+
+					return data;
+				}
 			}
 
-			window.addEventListener('load', function(event) {
-				var data = getLittlebugPageData();
-				if (data.Littledata.hasLittledataLayer) {
-					window.dispatchEvent(
-						new CustomEvent('setLittledataPageData', {
-							detail: data,
-						})
-					);
-				}
+			window.addEventListener('load', () => {
+				getLittlebugPageData();
 
-				var logLD = 'LD extension:' + JSON.stringify(data, null, 2);
-				console.debug(logLD);
 			});
 
 			// setting an internal visitor filter for GA
@@ -233,7 +244,9 @@ function injectContentScriptJS() {
 function initPageDataContentListener() {
 	let pageLog = [];
 	chrome.storage.local.get('pageLog', function(result) {
-		pageLog = JSON.parse(result.pageLog);
+
+		if (result.pageLog) pageLog = JSON.parse(result.pageLog);
+
 	});
 
 	window.addEventListener(
